c358368bcb623f9971d936fc4458b7e2c953b23f
=Kaladevi N S <kaladevi9@gmail.com>
2026-06-06 13:45:48 +0530

    env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: "https://crud-app-3-tchf.onrender.com",
 });
 
 export default API;
